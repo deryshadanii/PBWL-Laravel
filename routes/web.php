dbdaa1d7a2a4ac057acf5cc43953793d5e62b4b1
deryshadanii <deryshadani04@gmail.com>
2025-09-24 20:37:50 +0700

    Project 2

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/profil', function () {
+    return view('profil');
+});
